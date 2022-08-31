7bddd2bf4b75cb60fe1d3f93678c1f1b0543440e
kimseungbeom <kimsb7218@naver.com>
2022-08-31 18:06:07 +0900

    feat : Add SwaggerConfig

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.example.board.response.Response;
 import com.example.board.responsedto.BoardEditRequestDto;
 import com.example.board.responsedto.BoardRequestDto;
 import com.example.board.service.BoardService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +16,38 @@ import javax.validation.Valid;
 public class BoardController {
     private final BoardService boardService;
 
+    @ApiOperation(value = "전체 게시글 조회", notes = "전체 게시글을 조회합니다.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/board")
-    public Response getBoards(){
+    public Response getBoards() {
         return Response.success(boardService.getBoards());
     }
 
+    @ApiOperation(value = "단건 게시글 조회", notes = "단건 게시글을 조회합니다.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/board/{id}")
-    public Response getBoard(@PathVariable("id") Long id)
-    {
+    public Response getBoard(@PathVariable("id") Long id) {
         return Response.success(boardService.getBoard(id));
     }
 
+    @ApiOperation(value = "게시글 작성", notes = "게시글을 작성합니다.")
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/board")
-    public Response save(@Valid @RequestBody BoardRequestDto boardReq)
-    {
+    public Response save(@Valid @RequestBody BoardRequestDto boardReq) {
         return Response.success(boardService.save(boardReq));
     }
 
+    @ApiOperation(value = "게시글 수정", notes = "게시글을 수정합니다.")
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/board/{id}")
-    public Response editBoard(@PathVariable("id") Long id, @Valid @RequestBody BoardEditRequestDto req)
-    {
-        return Response.success(boardService.editBoard(id,req));
+    public Response editBoard(@PathVariable("id") Long id, @Valid @RequestBody BoardEditRequestDto req) {
+        return Response.success(boardService.editBoard(id, req));
     }
+
+    @ApiOperation(value = "게시글 삭제", notes = "게시글을 삭제합니다.")
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/board/{id}")
-    public Response deleteBoard(@PathVariable("id") Long id)
-    {
+    public Response deleteBoard(@PathVariable("id") Long id) {
         boardService.deleteBoard(id);
         return Response.success("삭제 완료");
     }
