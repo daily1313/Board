f5d48c8600282c86391ac1314dd59ee65013c0bc
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-10-11 12:50:19 +0900

    refactor : BoardControllerTest

--- src/test/java/com/example/board/BoardControllerTest.java
+++ src/test/java/com/example/board/BoardControllerTest.java
@@ -2,6 +2,7 @@ package com.example.board;
 
 
 import com.example.board.controller.BoardController;
+import com.example.board.responsedto.BoardEditRequestDto;
 import com.example.board.responsedto.BoardRequestDto;
 import com.example.board.service.BoardService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,8 +18,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,7 +47,7 @@ public class BoardControllerTest {
 
         //when
         mockMvc.perform(
-                post("/board")
+                post("/api/board")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(req)))
                 .andExpect(status().isOk());
@@ -54,5 +56,65 @@ public class BoardControllerTest {
         verify(boardService).save(req);
 
     }
+    @Test
+    @DisplayName("전체 게시글 조회 테스트")
+    public void getBoardsTest() throws Exception {
+        //given x
 
+        //when
+        mockMvc.perform(
+                get("/api/board")
+            ).andExpect(status().isOk());
+
+        //then
+        verify(boardService).getBoards();
+    }
+
+    @Test
+    @DisplayName("단건 게시글 조회 테스트")
+    public void getBoardTest() throws Exception {
+        //given
+        Long id = 1L;
+
+        //when
+        mockMvc.perform(
+                get("/api/board/{id}",id)
+        ).andExpect(status().isOk());
+
+        //then
+        verify(boardService).getBoard(id);
+    }
+
+    @Test
+    @DisplayName("게시글 수정 테스트")
+    public void editBoardTest() throws Exception {
+        //given
+        Long id = 1L;
+        BoardEditRequestDto req = new BoardEditRequestDto("제목","내용","작성자");
+
+        //when
+        mockMvc.perform(
+                put("/api/board/{id}",id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(req))
+        ).andExpect(status().isOk());
+
+        //then
+        verify(boardService).editBoard(id,req);
+    }
+
+    @Test
+    @DisplayName("게시글 삭제 테스트")
+    public void deleteBoardTest() throws Exception {
+        //given
+        Long id = 1L;
+
+        //when
+        mockMvc.perform(
+                delete("/api/board/{id}",id)
+        ).andExpect(status().isOk());
+
+        //then
+        verify(boardService).deleteBoard(id);
+    }
 }
