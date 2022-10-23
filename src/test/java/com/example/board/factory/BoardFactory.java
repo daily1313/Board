6eeca918730334d7e5930aa7711d3e6491362a84
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-10-23 16:52:47 +0900

    feat : BoardFactory

---
+++ src/test/java/com/example/board/factory/BoardFactory.java
@@ -0,0 +1,20 @@
+package com.example.board.factory;
+
+import com.example.board.entity.Board;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class BoardFactory {
+
+    public static Board createBoard() {
+        return new Board("title", "writer", "content");
+    }
+
+    public static Board createBoard(Long id) {
+
+        return new Board(id, "title", "writer", "content");
+    }
+}
