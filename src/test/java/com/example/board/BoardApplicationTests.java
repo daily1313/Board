50ee2e062e6277886f88975d242e24b6c4933b91
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-10-23 17:03:06 +0900

    refactor : BoardApplicationTests

--- src/test/java/com/example/board/BoardApplicationTests.java
+++ src/test/java/com/example/board/BoardApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("Test")
 class BoardApplicationTests {
 
     @Test
